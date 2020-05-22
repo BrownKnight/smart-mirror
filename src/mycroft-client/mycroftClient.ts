863b1075f9684da2b4b0c52a7cab852a065566e8
BrownKnight <amanrk@hotmail.co.uk>
2020-05-22 23:33:21 +0100

    Implement message hanbdling logix

--- src/mycroft-client/mycroftClient.ts
+++ src/mycroft-client/mycroftClient.ts
@@ -8,6 +8,15 @@ export type WebMusicControlMessage = {
     };
 };
 
+export type AudioControlMessage = {
+    type: string;
+};
+
+export type GenericMessage = {
+    type: string;
+    data: Record<string, any>;
+};
+
 export default class MycroftClient {
     private static instance: MycroftClient;
 
@@ -18,6 +27,8 @@ export default class MycroftClient {
 
     private socket: WebSocket;
     private webMusicControlEventHandlers: Map<string, Array<(message: WebMusicControlMessage) => void>> = new Map();
+    private audioControlEventHandlers: Map<string, Array<(message: AudioControlMessage) => void>> = new Map();
+    private genericEventHandlers: Map<string, Array<(message: GenericMessage) => void>> = new Map();
 
     private constructor() {
         this.socket = new WebSocket(URL);
@@ -25,34 +36,98 @@ export default class MycroftClient {
         this.registerDefaultHandlers();
     }
 
+    // SOCKET HANDLERS
+
     private registerDefaultHandlers() {
         this.socket.addEventListener("message", event => {
             const data = JSON.parse(event.data);
             // console.log(data);
             if ("type" in data) {
-                const [messageSender, messageType] = data.type.split(":");
-                if (messageSender === "web-music-control") {
-                    console.log(`Received message from ${messageSender} with type ${messageType}`);
-                    this.handleWebMusicControlMessage(messageType, data);
+                if (data.type.includes(":")) {
+                    const [messageSender, messageType] = data.type.split(":");
+                    if (messageSender === "web-music-control") {
+                        this.handleWebMusicControlMessage(messageType, data);
+                        return;
+                    }
                 }
+
+                if (data.type.includes("mycroft.audio.service")) {
+                    const messageType = data.type.split(".").pop();
+                    this.handleAudioControlMessage(messageType, data);
+                    return;
+                }
+
+                this.handleGenericMessage(data.type, data);
             }
         });
     }
 
+    sendMessage(messageType: string, data: Record<string, any>) {
+        console.log(`Sending message of type ${messageType}`);
+        this.socket.send(JSON.stringify({ type: messageType, data: data }));
+    }
+
+    // EVENT HANDLERS
+
     private handleWebMusicControlMessage(messageType: string, message: WebMusicControlMessage) {
         const handlers = this.webMusicControlEventHandlers.get(messageType);
         if (handlers) {
+            console.log(`Handling Web Music Control Message of type ${messageType}`);
             handlers.forEach(handler => handler(message));
+        } else {
+            console.log(`Web Music Control Event Handler not available for ${messageType}`);
         }
     }
 
-    on(eventName: string, eventHandler: (message: WebMusicControlMessage) => void) {
-        console.debug(`Registering event handler for ${eventName}`);
+    private handleAudioControlMessage(messageType: string, message: AudioControlMessage) {
+        const handlers = this.audioControlEventHandlers.get(messageType);
+        if (handlers) {
+            console.log(`Handling Audio Control Message of type ${messageType}`);
+            handlers.forEach(handler => handler(message));
+        } else {
+            console.log(`Audio Control Event Handler not available for ${messageType}`);
+        }
+    }
+
+    private handleGenericMessage(messageType: string, message: GenericMessage) {
+        const handlers = this.genericEventHandlers.get(messageType);
+        if (handlers) {
+            console.log(`Handling generic Message of type ${messageType}`);
+            handlers.forEach(handler => handler(message));
+        } else {
+            console.log(`Generic Handler not available for ${messageType}`);
+        }
+    }
+
+    // EVENT HANDLER REGISTRATION
+
+    onWebMusicControlMessage(eventName: string, eventHandler: (message: WebMusicControlMessage) => void) {
+        console.debug(`Registering music playback event handler for ${eventName}`);
         const handlers = this.webMusicControlEventHandlers.get(eventName);
         if (handlers) {
             handlers.push(eventHandler);
         } else {
             this.webMusicControlEventHandlers.set(eventName, [eventHandler]);
+        }
+    }
+
+    onAudioControlMessage(eventName: string, eventHandler: (message: AudioControlMessage) => void) {
+        console.debug(`Registering audio control event handler for ${eventName}`);
+        const handlers = this.audioControlEventHandlers.get(eventName);
+        if (handlers) {
+            handlers.push(eventHandler);
+        } else {
+            this.audioControlEventHandlers.set(eventName, [eventHandler]);
+        }
+    }
+
+    onGenericMessage(eventName: string, eventHandler: (message: GenericMessage) => void) {
+        console.debug(`Registering generic event handler for ${eventName}`);
+        const handlers = this.genericEventHandlers.get(eventName);
+        if (handlers) {
+            handlers.push(eventHandler);
+        } else {
+            this.genericEventHandlers.set(eventName, [eventHandler]);
         }
     }
 }
